b863a0fdea59fefbe9efa99a93d089a2217877ba
YAMADA Masaki <masaki@metabolics.co.jp>
2013-10-09 12:27:39 +0900

    install mongodb plugin

--- awe9/grails-app/conf/BuildConfig.groovy
+++ awe9/grails-app/conf/BuildConfig.groovy
@@ -68,5 +68,8 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
+
+	// custom installed plugins
+	compile ":mongodb:1.3.0"
     }
 }
